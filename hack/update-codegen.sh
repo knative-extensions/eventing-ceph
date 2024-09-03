8385b656c286a54c8eda7e925acdb8cc7f2c89c7
Matthias Wessendorf <mwessend@redhat.com>
2024-09-03 11:06:57 +0200

    Migrate to kube_gen (#509)
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -19,6 +19,7 @@ set -o nounset
 set -o pipefail
 
 source $(dirname $0)/../vendor/knative.dev/hack/codegen-library.sh
+source "${CODEGEN_PKG}/kube_codegen.sh"
 
 # If we run with -mod=vendor here, then generate-groups.sh looks for vendor files in the wrong place.
 export GOFLAGS=-mod=
@@ -29,14 +30,16 @@ echo "GOPATH=$GOPATH"
 
 group "Kubernetes Codegen"
 
-# generate the code with:
-# --output-base    because this script should also be able to run inside the vendor dir of
-#                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
-#                  instead of the $GOPATH directly. For normal projects this can be dropped.
-${CODEGEN_PKG}/generate-groups.sh "deepcopy,client,informer,lister" \
-  "knative.dev/eventing-ceph/pkg/client" "knative.dev/eventing-ceph/pkg/apis" \
-  "sources:v1alpha1" \
-  --go-header-file ${REPO_ROOT_DIR}/hack/boilerplate.go.txt
+kube::codegen::gen_helpers \
+  --boilerplate "${REPO_ROOT_DIR}/hack/boilerplate.go.txt" \
+  "${REPO_ROOT_DIR}/pkg/apis"
+
+kube::codegen::gen_client \
+  --boilerplate "${REPO_ROOT_DIR}/hack/boilerplate.go.txt" \
+  --output-dir "${REPO_ROOT_DIR}/pkg/client" \
+  --output-pkg "knative.dev/eventing-ceph/pkg/client" \
+  --with-watch \
+  "${REPO_ROOT_DIR}/pkg/apis"
 
 group "Knative Codegen"
 
